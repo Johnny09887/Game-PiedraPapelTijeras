b8b409c83b8a59c229f5a72fd5e02e364e85756b
Johnny09887 <andercando_2000@hotmail.com>
2024-10-16 21:32:22 -0500

    Implement logic to play a single round
    - Write the logic to play one round of the game
    - Modify existing code to integrate single-round functionality

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
+/*Paso 4: Declarar las variables de puntuación de los jugadores*/
+let humanScore = 0;
+let computerScore = 0;
+
 /*Paso 2:Código para obtener la elección de la computadora*/
 function getComputerChoice() {
   let randomInt = Math.floor(Math.random() * 3);
   if (randomInt === 0) {
-    return "Piedra";
+    return "piedra";
   } else if (randomInt === 1) {
-    return "Papel";
+    return "papel";
   } else {
-    return "Tijeras";
+    return "tijera";
   }
 }
-console.log(getComputerChoice());
 
 /*Paso 3:Código para obtener la elección humana*/
 function getHumanChoice() {
-  let userChoice = prompt("Escribe piedra, papel o tijera").toLocaleLowerCase();
+  let userChoice = prompt("Escribe piedra, papel o tijera").toLowerCase();
 
   if (
     userChoice === "piedra" ||
     userChoice === "papel" ||
-    userChoice === "tijeras"
+    userChoice === "tijera"
   ) {
     return userChoice;
   } else {
@@ -26,4 +29,31 @@ function getHumanChoice() {
     return getHumanChoice();
   }
 }
-console.log(getHumanChoice());
+
+/*Paso 5: Escribe la lógica para jugar una sola ronda*/
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+
+  console.log("Humano elige: " + humanChoice);
+  console.log("Computadora elige: " + computerChoice);
+
+  if (humanChoice === computerChoice) {
+    console.log(`¡Empate! Ambos eligieron ${humanChoice}`);
+  } else if (
+    (humanChoice === "piedra" && computerChoice === "tijera") ||
+    (humanChoice === "papel" && computerChoice === "piedra") ||
+    (humanChoice === "tijera" && computerChoice === "papel")
+  ) {
+    humanScore++;
+    console.log(`¡Ganastes! ${humanChoice} le gana a ${computerChoice}`);
+  } else {
+    computerScore++;
+    console.log(`¡Perdiste! ${computerChoice} le gana a ${humanChoice}`);
+  }
+  console.log(
+    `Puntuación actual: Humano ${humanScore} Computadora ${computerScore}`
+  );
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
